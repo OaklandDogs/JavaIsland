f74105bf80ae30836101bf7b1ee46c03cac1667a
OaklandDogs <116426066+OaklandDogs@users.noreply.github.com>
2023-01-26 21:03:00 +0500

    Update 1Lecture.java

--- 1Lecture.java
+++ 1Lecture.java
@@ -22,3 +22,37 @@ class Main
 }
 
 
+
+// Lecture.java
+
+// Написать метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true, 
+// не високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный
+
+import java.util.Scanner;
+
+public class Lecture {
+
+    public static void main(String[] args) {
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.println("Type a year: ");
+
+            int year = Integer.parseInt(reader.nextLine());
+
+            if (year % 400 == 0 && year % 100 == 0 && year % 4 == 0) {
+                System.out.println("The year is a leap year");
+            } else
+             if (year % 4 == 0 && year%100!=0 ) {
+                System.out.println("The year is a leap year");
+            } else 
+            {
+                System.out.println("The year is not a leap year");
+            }
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
