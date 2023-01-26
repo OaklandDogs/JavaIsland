ce7fa3b4744ae18aafb0dbc6749d7b78aecadce5
OaklandDogs <116426066+OaklandDogs@users.noreply.github.com>
2023-01-26 21:09:34 +0500

    Create 2Lecture.java

---
+++ 2Lecture.java
@@ -0,0 +1,42 @@
+// Main.java / Name
+
+public class Main {
+    public static final String TEXT = "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesetest";
+    public static final String PATTERN = "test";
+    
+    public static void main(String[] args) {
+        int count = 0;
+    
+        for (int i = 0; i < TEXT.length(); i++) {
+           char ch = TEXT.charAt(i);
+          
+         
+          for (int a = 0; a < PATTERN.length(); a++) {
+            char c1 = PATTERN.charAt(a);
+            if (ch == c1) {
+            count++;
+            }
+        }
+          }
+    
+        System.out.println("Строка " + PATTERN + " встретилась в тексте " + count + " раз");
+    }
+}
+
+
+// pal.java
+
+// Напишите метод, который принимает на вход строку (String) 
+// и определяет является ли строка палиндромом (возвращает boolean значение).
+
+public class pal {
+    public static void main (String[] args) {
+        String s = " z  --abc  67876 ..cba Z";
+        s = s.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+        if (s.toLowerCase().equals((new StringBuilder(s)).reverse().toString().toLowerCase()))
+            System.out.println("Палиндром! " + s);
+        else
+            System.out.println("Не палиндром! :( " + s);
+        }
+    }
+
